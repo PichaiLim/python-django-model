8b1750c62383b648d3f9fa9727ec2a64b4c83042
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2018-08-10 23:55:40 +0700

    4.2. The end model in admin

--- src/blog/models.py
+++ src/blog/models.py
@@ -3,12 +3,16 @@ from django.db import models
 
 # Create your models here.
 class PostModel(models.Model):
+    # https://docs.djangoproject.com/en/2.1/topics/db/models/#fields
     id = models.BigAutoField(primary_key=True)
     active = models.BooleanField(default=True) #empty in the database
     title = models.CharField(max_length=240, verbose_name='Post title') # 'verbose_name' => change label name
     content =  models.TextField(null=True, blank=True)
-    pass
-    class Metal:
+    
+    # Disable 'pass' for using 'class Metal'
+
+    # https://docs.djangoproject.com/en/2.1/ref/models/options/
+    class Metal: 
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
 
