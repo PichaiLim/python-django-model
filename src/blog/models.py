a9b6c078594f07ce3b83cb76438c41b3347984b6
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2018-08-10 23:40:17 +0700

    4. Render models in admin

--- src/blog/models.py
+++ src/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 # from django.db.models import Model
 
 # Create your models here.
-class Post(models.Model):
+class PostModel(models.Model):
     id = models.BigAutoField(primary_key=True)
     active = models.BooleanField(default=True) #empty in the database
     title = models.CharField(max_length=240)
