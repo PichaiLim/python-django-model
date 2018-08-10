79bd1cf9b1a146f64bdcc83c91859f3cd9441100
Pichai Limpanitivat <Pichai.Limpanitivat@Gmail.com>
2018-08-10 23:43:56 +0700

    4.1. commit change 'Title name', 'Label name', 'Breadcrumbs name'

--- src/blog/models.py
+++ src/blog/models.py
@@ -5,9 +5,12 @@ from django.db import models
 class PostModel(models.Model):
     id = models.BigAutoField(primary_key=True)
     active = models.BooleanField(default=True) #empty in the database
-    title = models.CharField(max_length=240)
+    title = models.CharField(max_length=240, verbose_name='Post title') # 'verbose_name' => change label name
     content =  models.TextField(null=True, blank=True)
     pass
+    class Metal:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
 
 
 
